a41e1ac411e39e03e59af3136d035f1300defa39
Debbie <86463161+qingz1231@users.noreply.github.com>
2023-09-11 06:45:47 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.chzc1kwrw7sr.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "debbielowqingyi-employee"
+customregion = "us-east-1"
+
